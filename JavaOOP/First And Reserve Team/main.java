8332d5f2898ee3305fc06b7419a2d011e2b83dcd
Ivan Ivanov <vankodj@students.softuni.bg>
2023-01-07 21:59:57 +0200

    Added First And Reserve Team

---
+++ JavaOOP/First And Reserve Team/main.java
@@ -0,0 +1,28 @@
+package firstAndReserveTeam;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        Team team = new Team("Black Eagles");
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person personToAdd = new Person(firstName, lastName, age, salary);
+            team.addPlayer(personToAdd);
+
+        }
+//First team have 4 players
+//Reserve team have 1 players
+        System.out.printf("First team have %d players%n", team.getFirstTeam().size());
+        System.out.printf("Reserve team have %d players", team.getReserveTeam().size());
+
+    }
+}
