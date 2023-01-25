ec84f1bed5e58cba417fe4bfd57230446409fe16
Ivan Ivanov <vankodj@students.softuni.bg>
2023-01-25 12:32:38 +0200

    Added Math Operation

---
+++ JavaOOP/Math Operation/Main.java
@@ -0,0 +1,11 @@
+package mathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MathOperation mathOperation = new MathOperation();
+
+        System.out.println(mathOperation.add(2,3));
+
+    }
+}
