58162104d9faa2224be3209e5c5ef3fc6dfb3229
Ivan Ivanov <vankodj@students.softuni.bg>
2023-01-31 21:54:58 +0200

    Added Create Tables

---
+++ Java DB/01.Exersice - Create Tables.sql
@@ -0,0 +1,8 @@
+CREATE table `minions`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50),
+`age` INT);  
+CREATE table `towns`(
+`town_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50)
+);
