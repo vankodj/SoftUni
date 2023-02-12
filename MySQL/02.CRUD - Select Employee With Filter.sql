976660fde770eee19d62e3bc81ce03ee9bf9e25a
Ivan Ivanov <vankodj@students.softuni.bg>
2023-02-12 17:28:43 +0200

    Modifying

---
+++ MySQL/02.CRUD - Select Employee With Filter.sql
@@ -0,0 +1,6 @@
+SELECT `id`, CONCAT_WS(" ",`first_name`,`last_name`) AS "Full Name",
+  `job_title` AS "Job Title",
+  `salary` AS "Salary"
+  FROM `employees`
+  WHERE `salary` >1000
+  ORDER BY `id`;
