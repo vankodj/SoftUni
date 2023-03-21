24e96eca8eace30dc3c623aaddd3315addf2aa98
Ivan Ivanov <vankodj@students.softuni.bg>
2023-03-21 18:38:05 +0200

    Added Mountains and Peaks

---
+++ MySQL/Table Relations/1. Mountains and Peaks.sql
@@ -0,0 +1,13 @@
+CREATE table `mountains`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL
+);
+
+CREATE table `peaks`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`mountain_id` INT,
+ CONSTRAINT fk_peak_mountain
+FOREIGN KEY (`mountain_id`)
+REFERENCES  `mountains`(`id`)
+);
