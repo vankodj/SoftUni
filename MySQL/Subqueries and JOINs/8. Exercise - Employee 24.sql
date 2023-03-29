25e964010a1c92f163b5f1483017187ce2328c4d
Ivan Ivanov <vankodj@students.softuni.bg>
2023-03-29 13:10:28 +0300

    Added Employee 24

---
+++ MySQL/Subqueries and JOINs/8. Exercise - Employee 24.sql
@@ -0,0 +1,6 @@
+SELECT  `e`.`employee_id`,`e`.`first_name`,IF(YEAR( p.`start_date`) >=2005,NULL,`p`.`name`)
+FROM `employees` AS e
+JOIN `employees_projects` AS ep ON `ep`.`employee_id` = `e`.`employee_id`
+JOIN `projects` AS p ON `ep`.`project_id` = `p`.`project_id`
+WHERE e.`employee_id` = 24
+ORDER BY `p`.`name` ASC
