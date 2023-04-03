79276a3b5fd4367a2218e31c8ca76ec7a55845a2
Ivan Ivanov <vankodj@students.softuni.bg>
2023-04-03 19:40:36 +0300

    Added Employees with Salary Above Number

--- MySQL/Database Programmability/2.Exercise - Employees with Salary Above Number.sql
+++ MySQL/Database Programmability/2.Exercise - Employees with Salary Above Number.sql
@@ -1,7 +1,7 @@
-CREATE PROCEDURE usp_get_employees_salary_above_35000()
+CREATE PROCEDURE usp_get_employees_salary_above(desired_salary DECIMAL(19,4))
 BEGIN
   SELECT `first_name`, `last_name`
   FROM `employees`
-  WHERE `salary` > 35000
+  WHERE `salary` >= desired_salary
   ORDER BY `first_name`, `last_name`, `employee_id`;
 END
