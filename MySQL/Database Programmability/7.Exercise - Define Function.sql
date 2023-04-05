dc7dfeefa0181351186bfa2fcdf18244ee09441f
Ivan Ivanov <vankodj@students.softuni.bg>
2023-04-05 19:14:46 +0300

    Added Define Function

---
+++ MySQL/Database Programmability/7.Exercise - Define Function.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+RETURNS int
+DETERMINISTIC
+BEGIN
+
+RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+END
