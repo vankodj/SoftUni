b943ac92850994d751ca78f97c8a12fb52654506
Ivan Ivanov <vankodj@students.softuni.bg>
2023-04-11 17:27:03 +0300

    Added Movie awards

---
+++ MySQL/ExamPrep/11. Movie awards.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE udp_award_movie(movie_title VARCHAR(50))
+ BEGIN
+ UPDATE `actors` AS a
+ JOIN `movies_actors` AS ma ON ma.`actor_id` = a.`id`
+ JOIN `movies` AS m ON m.`id` = ma.`movie_id`
+ SET a.`awards` = a.`awards` +1
+ WHERE m.`title` = `movie_title`;
+ END
