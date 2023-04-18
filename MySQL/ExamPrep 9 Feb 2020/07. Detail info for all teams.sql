62ddcb184e9d08f0872a9c884515fbec4fd3beba
Ivan Ivanov <vankodj@students.softuni.bg>
2023-04-18 23:13:51 +0300

    Added Detail info for all teams

---
+++ MySQL/ExamPrep 9 Feb 2020/07. Detail info for all teams.sql
@@ -0,0 +1,6 @@
+SELECT t.`name` AS 'team_name',t.`established`,t.`fan_base`,
+COUNT(p.`id`) AS 'players_count'
+FROM `teams` AS t
+LEFT JOIN `players` AS p ON p.`team_id` = t.`id`
+GROUP BY t.`id`
+ORDER BY `players_count` DESC, t.`fan_base` DESC;
