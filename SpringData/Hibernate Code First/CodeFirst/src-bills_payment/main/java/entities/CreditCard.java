ee11e17b78ff134a571506ddc7e72b306c177a2c
Ivan Ivanov <vankodj@students.softuni.bg>
2023-05-10 19:00:02 +0300

    Add

---
+++ SpringData/Hibernate Code First/CodeFirst/src-bills_payment/main/java/entities/CreditCard.java
@@ -0,0 +1,46 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class CreditCard extends BaseEntity {
+
+    @Column
+    private String cardType;
+
+    @Column(name = "expiration_month")
+    private byte expirationMonth;
+
+    @Column(name = "expiration_year")
+    private short expirationYear;
+
+    public CreditCard() {
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public byte getExpirationMonth() {
+        return expirationMonth;
+    }
+
+    public void setExpirationMonth(byte expirationMonth) {
+        this.expirationMonth = expirationMonth;
+    }
+
+    public short getExpirationYear() {
+        return expirationYear;
+    }
+
+    public void setExpirationYear(short expirationYear) {
+        this.expirationYear = expirationYear;
+    }
+}
