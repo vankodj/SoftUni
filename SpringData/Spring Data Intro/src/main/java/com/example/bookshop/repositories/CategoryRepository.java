86e8891603a4202b8222f21cfb9b868191447c1f
Ivan Ivanov <vankodj@students.softuni.bg>
2023-05-16 23:11:42 +0300

    Add

---
+++ SpringData/Spring Data Intro/src/main/java/com/example/bookshop/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookshop.repositories;
+
+import com.example.bookshop.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+}
