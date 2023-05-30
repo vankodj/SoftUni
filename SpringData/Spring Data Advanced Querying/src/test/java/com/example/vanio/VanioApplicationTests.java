02f11d26fd094a96079cd3e016b33907ce442789
Ivan Ivanov <vankodj@students.softuni.bg>
2023-05-30 22:57:49 +0300

    Add

---
+++ SpringData/Spring Data Advanced Querying/src/test/java/com/example/vanio/VanioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vanio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
