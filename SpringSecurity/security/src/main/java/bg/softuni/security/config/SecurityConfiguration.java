90740d493bfd373cb8999e4b3343f151cfad694d
Ivan Ivanov <cant_u_see@abv.bg>
2023-09-26 17:44:42 +0300

    Added login and logout logic

--- SpringSecurity/security/src/main/java/bg/softuni/security/config/SecurityConfiguration.java
+++ SpringSecurity/security/src/main/java/bg/softuni/security/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package bg.softuni.security.config;
 
+import bg.softuni.security.model.enums.UserRoleEnum;
 import bg.softuni.security.repository.UserRepository;
 import bg.softuni.security.service.AppUserDetailsService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
@@ -18,9 +21,37 @@ public class SecurityConfiguration {
         return new Pbkdf2PasswordEncoder("",256,256,2);
     }
 
+//    enable or disable static resources and login,home,registration pages
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
-    return null;
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().requestMatchers(PathRequest
+                        .toStaticResources()
+                        .atCommonLocations())
+                .permitAll()
+//                everyone can register and login
+                .requestMatchers("/", "/users/login", "/users/register").permitAll()
+//                pages for moderators
+                .requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name())
+//                pages for admins
+                .requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name())
+                .anyRequest().authenticated()
+//                configuration of form login
+             .and()
+                .formLogin().loginPage("/users/login")
+                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
+//                where to go when login successful
+                .defaultSuccessUrl("/")
+//                where to go when we have incorrect login
+                .failureForwardUrl("/users/login-error")
+             .and()
+                .logout().logoutUrl("users/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+
+
+
+    return http.build();
     }
 
     @Bean
