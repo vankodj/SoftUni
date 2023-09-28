6a407cfbefebc62398e5b397557ab71405e2b376
Ivan Ivanov <cant_u_see@abv.bg>
2023-09-28 15:07:50 +0300

    Basic Registration form

--- SpringSecurity/security/src/main/java/bg/softuni/security/service/UserService.java
+++ SpringSecurity/security/src/main/java/bg/softuni/security/service/UserService.java
@@ -1,10 +1,15 @@
 package bg.softuni.security.service;
 
+import bg.softuni.security.model.dto.UserRegisterDTO;
 import bg.softuni.security.model.entity.UserEntity;
 import bg.softuni.security.model.entity.UserRoleEntity;
 import bg.softuni.security.model.enums.UserRoleEnum;
 import bg.softuni.security.repository.UserRepository;
 import bg.softuni.security.repository.UserRoleRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,28 +20,31 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
-
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserRoleRepository userRoleRepository, PasswordEncoder passwordEncoder) {
+    private final UserDetailsService userDetailsService;
+
+    public UserService(UserRepository userRepository, UserRoleRepository userRoleRepository, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
         this.userRepository = userRepository;
         this.userRoleRepository = userRoleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
     }
 
-    public void init(){
-     if (userRepository.count() == 0 && userRoleRepository.count() == 0){
-         UserRoleEntity adminRole = new UserRoleEntity().setUserRole(UserRoleEnum.ADMIN);
-         UserRoleEntity moderatorRole = new UserRoleEntity().setUserRole(UserRoleEnum.MODERATOR);
-         adminRole = userRoleRepository.save(adminRole);
-         moderatorRole = userRoleRepository.save(moderatorRole);
+    public void init() {
+        if (userRepository.count() == 0 && userRoleRepository.count() == 0) {
+            UserRoleEntity adminRole = new UserRoleEntity().setUserRole(UserRoleEnum.ADMIN);
+            UserRoleEntity moderatorRole = new UserRoleEntity().setUserRole(UserRoleEnum.MODERATOR);
+            adminRole = userRoleRepository.save(adminRole);
+            moderatorRole = userRoleRepository.save(moderatorRole);
 
-         initAdmin(List.of(adminRole,moderatorRole));
-         initModerator(List.of(moderatorRole));
-         initUser(List.of());
-     }
+            initAdmin(List.of(adminRole, moderatorRole));
+            initModerator(List.of(moderatorRole));
+            initUser(List.of());
+        }
     }
-    private void initAdmin(List<UserRoleEntity> roles){
+
+    private void initAdmin(List<UserRoleEntity> roles) {
         UserEntity admin = new UserEntity()
                 .setUserRoles(roles)
                 .setFirstName("Admin")
@@ -46,7 +54,8 @@ public class UserService {
         userRepository.save(admin);
 
     }
-    private void initModerator(List<UserRoleEntity> roles){
+
+    private void initModerator(List<UserRoleEntity> roles) {
         UserEntity moderator = new UserEntity()
                 .setUserRoles(roles)
                 .setFirstName("Moderator")
@@ -57,7 +66,8 @@ public class UserService {
         userRepository.save(moderator);
 
     }
-    private void initUser(List<UserRoleEntity> roles){
+
+    private void initUser(List<UserRoleEntity> roles) {
         UserEntity user = new UserEntity()
                 .setUserRoles(roles)
                 .setFirstName("User")
@@ -67,6 +77,27 @@ public class UserService {
 
         userRepository.save(user);
 
+    }
+
+    public void registerAndLogin(UserRegisterDTO userRegisterDTO) {
+        UserEntity newUser = new UserEntity()
+                .setFirstName(userRegisterDTO.getFirstName())
+                .setLastName(userRegisterDTO.getLastName())
+                .setEmail(userRegisterDTO.getEmail())
+                .setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
+        userRepository.save(newUser);
+UserDetails userDetails =
+        userDetailsService.loadUserByUsername(newUser.getEmail());
+
+        UsernamePasswordAuthenticationToken auth =
+                 new UsernamePasswordAuthenticationToken(
+                        userDetails
+                ,userDetails.getPassword()
+                ,userDetails.getAuthorities()
+        );
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(auth);
     }
 
 }
--- SpringSecurity/security/src/main/java/bg/softuni/security/web/LoginController.java
+++ SpringSecurity/security/src/main/java/bg/softuni/security/web/LoginController.java
@@ -2,9 +2,6 @@ package bg.softuni.security.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
