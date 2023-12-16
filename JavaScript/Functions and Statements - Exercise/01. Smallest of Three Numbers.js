3440088f6d837c3b39c983a34f618c0eaff3e388
Ivan Ivanov <cant_u_see@abv.bg>
2023-12-16 19:48:37 +0200

    Add

---
+++ JavaScript/Functions and Statements - Exercise/01. Smallest of Three Numbers.js
@@ -0,0 +1,11 @@
+function smallestOfTreeNumbers(num1,num2,num3){
+if(num1<=num2 && num1<=num3){
+    console.log(num1);
+}else if(num2<=num1 && num2<=num3){
+    console.log(num2);
+}else if(num3<=num1 && num3<=num2){
+    console.log(num3);
+}
+
+}
+smallestOfTreeNumbers(2,5,3);
