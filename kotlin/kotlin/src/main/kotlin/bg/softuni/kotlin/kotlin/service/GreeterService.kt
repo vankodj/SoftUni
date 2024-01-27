922644d0be177ecc9075f259743c389e5527575f
Ivan Ivanov <cant_u_see@abv.bg>
2024-01-27 22:19:20 +0200

    Add

---
+++ kotlin/kotlin/src/main/kotlin/bg/softuni/kotlin/kotlin/service/GreeterService.kt
@@ -0,0 +1,11 @@
+package bg.softuni.kotlin.kotlin.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class GreeterService {
+
+    fun generateGreeting(name: String) : String{
+        return "Hello $name"
+    }
+}
