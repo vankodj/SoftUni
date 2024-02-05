e5b33bec19186a147bc1b6bb42f1c27edd0fba31
Ivan Ivanov <cant_u_see@abv.bg>
2024-02-05 22:29:21 +0200

    Add

---
+++ JavaScript/REST/06. Github-Commits/app.js
@@ -0,0 +1,26 @@
+function loadCommits() {
+    const username = document.getElementById("username");
+    const repo = document.getElementById("repo");
+    const commits = document.getElementById("commits");
+    const usernameValue = username.value;
+    const repoValue = repo.value;
+    const BASE_URL = "https://api.github.com/repos/";
+    fetch(`${BASE_URL}${usernameValue}/${repoValue}/commits`)
+    .then((res)=> res.json())
+    .then((data)=>{
+    
+    data.forEach(({commit}) => {
+        const li = document.createElement("li");
+        li.textContent = `${commit.author.name}: ${commit.message}`;
+        commits.appendChild(li);
+    });
+    }
+    ).catch((err) => {
+        const li = document.createElement("li");
+        li.textContent = err.message;
+        commits.appendChild(li);
+    })
+
+    console.log('TODO...');
+
+}
